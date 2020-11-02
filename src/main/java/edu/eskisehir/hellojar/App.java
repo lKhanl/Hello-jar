da1865ab0f45c662bc8b4e5a15f8c91de124a5dc
Khan <oguzhanercelik@eskisehir.edu.tr>
2020-11-02 16:40:23 +0300

    Hello firs executable jar project!

---
+++ src/main/java/edu/eskisehir/hellojar/App.java
@@ -0,0 +1,17 @@
+package edu.eskisehir.hellojar;
+
+import javax.swing.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        JOptionPane.showMessageDialog(null,"Hello World!");
+
+    }
+}
