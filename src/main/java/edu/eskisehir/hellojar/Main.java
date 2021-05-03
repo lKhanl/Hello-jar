6ae81dd533938ae96735a0a179ed88e69ec954eb
Khan <oguzhanercelik@eskisehir.edu.tr>
2021-05-03 15:19:00 +0300

    updated

--- src/main/java/edu/eskisehir/hellojar/Main.java
+++ src/main/java/edu/eskisehir/hellojar/Main.java
@@ -4,14 +4,11 @@ import javax.swing.*;
 
 /**
  * Hello world!
- *
  */
-public class Main
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        JOptionPane.showMessageDialog(null,"Hello World!");
-
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        JOptionPane.showMessageDialog(null, "Hello World!");
+        System.out.println("aaaaaaaaaaaaaaaa");
     }
 }
